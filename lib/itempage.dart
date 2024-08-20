245450561a0977e48bce5efba767bbfbd3c046b4
Sazidul islam <rabbiking00@gmail.com>
2024-08-21 02:12:28 +0600

    E-commerce project

--- lib/itempage.dart
+++ lib/itempage.dart
@@ -1,13 +1,22 @@
+import 'package:clippy_flutter/arc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'itemappber.dart';
 
 class itempage extends StatelessWidget {
+  List<Color> clrs = [
+    Colors.red,
+    const Color.fromARGB(255, 73, 100, 122),
+    Colors.yellow,
+    Colors.indigo,
+    Colors.black
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: ListView(
-        children: [
+        backgroundColor: Colors.blue,
+        body: ListView(children: [
           itemappber(),
           Padding(
             padding: EdgeInsets.all(16),
@@ -16,9 +25,243 @@ class itempage extends StatelessWidget {
               height: 300,
             ),
           ),
-          // Arc(edge: Edge.top),
-        ],
-      ),
-    );
+          Arc(
+            edge: Edge.TOP,
+            arcType: ArcType.CONVEY,
+            height: 30,
+            child: Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 50, bottom: 20),
+                        child: Row(
+                          children: [
+                            Text('Product Name',
+                                style: TextStyle(
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue,
+                                ))
+                          ],
+                        ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.only(top: 5, bottom: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              RatingBar.builder(
+                                initialRating: 4,
+                                minRating: 1,
+                                direction: Axis.horizontal,
+                                itemCount: 5,
+                                itemSize: 20,
+                                itemPadding:
+                                    EdgeInsets.symmetric(horizontal: 4),
+                                itemBuilder: (context, index) => Icon(
+                                  Icons.favorite,
+                                  color: Colors.blue,
+                                ),
+                                onRatingUpdate: (index) {},
+                              ),
+                              //Rating are pashe minus dewar jnno ai code---------------------------------------------------------
+                              Spacer(),
+                              Row(
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.all(4),
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(20),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.blue.withOpacity(0.5),
+                                            spreadRadius: 3,
+                                            blurRadius: 10,
+                                            offset: Offset(0, 3),
+                                          )
+                                        ]),
+                                    child: Icon(
+                                      CupertinoIcons.minus,
+                                      size: 18,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Container(
+                                margin: EdgeInsets.symmetric(horizontal: 8),
+                                child: Text(
+                                  '01',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.blue),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.all(1),
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(20),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.blue.withOpacity(0.5),
+                                            spreadRadius: 3,
+                                            blurRadius: 10,
+                                            offset: Offset(0, 3),
+                                          )
+                                        ]),
+                                    child: Icon(
+                                      CupertinoIcons.plus,
+                                      size: 18,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )),
+                      Padding(
+                        padding: EdgeInsets.all(12),
+                        child: Text(
+                          textAlign: TextAlign.justify,
+                          'This is more ditals discription hare, You can write your product domunets ETC, This is lenthy discription.',
+                          style: TextStyle(color: Colors.blue, fontSize: 17),
+                        ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8),
+                          child: Row(
+                            children: [
+                              Text(
+                                'Size',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Row(children: [
+                                for (int i = 5; i < 10; i++)
+                                  Container(
+                                    height: 30,
+                                    width: 30,
+                                    alignment: Alignment.center,
+                                    margin: EdgeInsets.symmetric(horizontal: 5),
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(30),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.blue.withOpacity(0.5),
+                                            blurRadius: 8,
+                                            spreadRadius: 2,
+                                          )
+                                        ]),
+                                    child: Text(
+                                      i.toString(),
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.blue),
+                                    ),
+                                  ),
+                              ])
+                            ],
+                          )),
+                      Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8),
+                          child: Row(
+                            children: [
+                              Text(
+                                'Color',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Row(children: [
+                                for (int i = 0; i < 5; i++)
+                                  Container(
+                                    height: 30,
+                                    width: 30,
+                                    alignment: Alignment.center,
+                                    margin: EdgeInsets.symmetric(horizontal: 5),
+                                    decoration: BoxDecoration(
+                                        color: clrs[i],
+                                        borderRadius: BorderRadius.circular(30),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.blue.withOpacity(0.5),
+                                            blurRadius: 8,
+                                            spreadRadius: 2,
+                                          )
+                                        ]),
+                                  ),
+                              ])
+                            ],
+                          ))
+                    ],
+                  )),
+            ),
+          ),
+        ]),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 70,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.blue.withOpacity(0.5),
+                    spreadRadius: 3,
+                    blurRadius: 10,
+                    offset: Offset(0, 3),
+                  ),
+                ]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '\$120',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    CupertinoIcons.cart_badge_plus,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Add to cart',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.blue),
+                      padding: MaterialStateProperty.all(
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 17))),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
